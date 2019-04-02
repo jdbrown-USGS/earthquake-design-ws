b4f3b00025866f219e9745331f26f247cbc75501
jdbrown-usgs <jdbrown@usgs.gov>
2019-04-02 11:37:55 -0600

    created new schema for risk-targeted

---
+++ src/lib/db/risk-targeted/schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE region (
+  id SERIAL NOT NULL PRIMARY KEY,
+
+  grid_spacing NUMERIC NOT NULL,
+  max_latitude NUMERIC NOT NULL,
+  max_longitude NUMERIC NOT NULL,
+  min_latitude NUMERIC NOT NULL,
+  min_longitude NUMERIC NOT NULL,
+  name VARCHAR(255) NOT NULL UNIQUE,
+  periods NUMERIC ARRAY DEFAULT NULL,
+  vs30 NUMERIC DEFAULT NULL
+);
+
+CREATE TABLE data (
+  id SERIAL NOT NULL PRIMARY KEY,
+  region_id INTEGER NOT NULL REFERENCES region(id) ON DELETE CASCADE,
+
+  latitude NUMERIC NOT NULL,
+  longitude NUMERIC NOT NULL,
+  pga NUMERIC DEFAULT NULL,
+  sa NUMERIC ARRAY DEFAULT NULL,
+  UNIQUE (region_id, latitude, longitude)
+);
+
+CREATE TABLE document (
+  id SERIAL NOT NULL PRIMARY KEY,
+  region_id INTEGER NOT NULL REFERENCES region(id) ON DELETE CASCADE,
+
+  name VARCHAR(255) NOT NULL,
+  UNIQUE (region_id, name)
+);
